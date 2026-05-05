742ed0dd6dc7835a1a9c225735f41fee2c5e825c
goszpeti <peter.gosztolya@gmail.com>
2026-05-05 21:32:15 +0200

    Fix website url

--- src/waqd_website/ui/android/app/src/main/java/com/waqd/app/WeatherWidgetProvider.java
+++ src/waqd_website/ui/android/app/src/main/java/com/waqd/app/WeatherWidgetProvider.java
@@ -36,7 +36,7 @@ import java.io.FileOutputStream;
 public class WeatherWidgetProvider extends AppWidgetProvider {
 
     private static final ExecutorService executor = Executors.newSingleThreadExecutor();
-    private static final String BASE_URL = "http://192.168.178.57:8000";
+    private static final String BASE_URL = "https://waqd.de";
     private static final String ACTION_WEATHER_UPDATE = "com.waqd.app.WEATHER_UPDATE";
 
     @Override
