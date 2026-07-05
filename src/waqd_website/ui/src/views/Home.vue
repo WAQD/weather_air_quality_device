ae78d7f3e4c9101ce3c470664f4f4c796b492bab
goszpeti <peter.gosztolya@gmail.com>
2026-07-05 11:44:22 +0200

    Update keystore file path and enhance button styles in Home.vue

--- src/waqd_website/ui/src/views/Home.vue
+++ src/waqd_website/ui/src/views/Home.vue
@@ -196,17 +196,25 @@
                     <p class="text-base sm:text-lg lg:text-xl mb-6 sm:mb-8">
                         {{ t('home_build_register') }}
                     </p>
-                    <div class="flex flex-col sm:flex-row gap-3 sm:gap-4 justify-center">
+                    <div class="flex flex-wrap gap-3 sm:gap-4 justify-center">
+                        <a href="https://github.com/WAQD/weather_air_quality_device/releases/latest/download/waqd-app-release.apk"
+                            class="btn btn-sm sm:btn-md lg:btn-lg whitespace-normal h-auto min-h-[3rem] py-2 bg-[#3DDC84] border-[#3DDC84] text-black hover:bg-[#2bb870] hover:border-[#2bb870]">
+                            <!-- Android head -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 shrink-0" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M17.6 9.48l1.84-3.18c.16-.31.04-.69-.26-.85a.637.637 0 0 0-.85.26l-1.87 3.23C14.96 8.17 13.54 7.8 12 7.8s-2.96.37-4.46 1.14L5.67 5.71a.637.637 0 0 0-.85-.26c-.3.16-.42.54-.26.85l1.84 3.18C3.6 11.07 1.96 13.6 1.96 16.4h20.08c0-2.8-1.64-5.33-4.44-6.92zM8.68 13.8a.8.8 0 1 1 0-1.6.8.8 0 0 1 0 1.6zm6.64 0a.8.8 0 1 1 0-1.6.8.8 0 0 1 0 1.6z"/>
+                            </svg>
+                            {{ t('home_download_android') }}
+                        </a>
                         <a href="https://github.com/WAQD/weather_air_quality_device/wiki/Assembly-and-Software-Setup-Guide"
-                            target="_blank" class="btn btn-secondary btn-sm sm:btn-md lg:btn-lg">
-                            <svg class="h-6 w-6 mr-2">
+                            target="_blank" class="btn btn-secondary btn-sm sm:btn-md lg:btn-lg whitespace-normal h-auto min-h-[3rem] py-2">
+                            <svg class="h-6 w-6 mr-2 shrink-0">
                                 <use :href="taskListIconUrl" fill="currentColor" />
                             </svg>
                             {{ t('home_view_assembly') }}
                         </a>
                         <a href="https://github.com/goszpeti/weather_air_quality_device"
-                            target="_blank" class="btn btn-secondary btn-sm sm:btn-md lg:btn-lg">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2"
+                            target="_blank" class="btn btn-secondary btn-sm sm:btn-md lg:btn-lg whitespace-normal h-auto min-h-[3rem] py-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 shrink-0"
                                 viewBox="0 0 24 24" fill="currentColor">
                                 <path
                                     d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
@@ -214,8 +222,8 @@
                             {{ t('home_view_github') }}
                         </a>
                         <router-link to="/public/about"
-                            class="btn btn-primary btn-secondary btn-sm sm:btn-md lg:btn-lg">
-                            <svg class="h-6 w-6 mr-2">
+                            class="btn btn-primary btn-secondary btn-sm sm:btn-md lg:btn-lg whitespace-normal h-auto min-h-[3rem] py-2">
+                            <svg class="h-6 w-6 mr-2 shrink-0">
                                 <use :href="infoIconUrl" fill="currentColor" />
                             </svg>
                             {{ t('settings_tab_about') }}
